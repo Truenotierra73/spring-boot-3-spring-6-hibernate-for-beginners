0a48cafd713ed55ffdf3830e720b749a3bcdb447
Agustin Bollati <agustin.bollati@fluxit.com.ar>
2025-08-13 17:32:33 -0300

    primer demo en spring boot

---
+++ 01-spring-boot-quickstart/01-myfirtsapp/src/main/java/com/agustinbollati/springboot/demo/myfirtsapp/rest/FunRestController.java
@@ -0,0 +1,14 @@
+package com.agustinbollati.springboot.demo.myfirtsapp.rest;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class FunRestController {
+
+    // expose "/" that return "Hello World"
+    @GetMapping("/")
+    public String sayHello() {
+        return "Hello World";
+    }
+}
