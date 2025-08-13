32fece530a470157fed45cd2f5c67c03d516b9b4
Agustin Bollati <agustin.bollati@fluxit.com.ar>
2025-08-13 17:58:44 -0300

    renombrar myfirstapp por spring-boot-demo y agregar dev-tools-demo

---
+++ 01-spring-boot-quickstart/02-dev-tools-demo/src/test/java/com/agustinbollati/springboot/demo/myfirtsapp/MyfirtsappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.agustinbollati.springboot.demo.myfirtsapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyfirtsappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
