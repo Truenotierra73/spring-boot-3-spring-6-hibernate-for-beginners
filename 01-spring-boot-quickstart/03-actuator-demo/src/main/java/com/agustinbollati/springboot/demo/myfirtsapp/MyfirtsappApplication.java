f49c6d4e3fc621f9d5f4da76276b8d0caf2902df
Agustin Bollati <bollatiagustin@gmail.com>
2025-08-15 18:32:06 -0300

    agregar actuator demo

---
+++ 01-spring-boot-quickstart/03-actuator-demo/src/main/java/com/agustinbollati/springboot/demo/myfirtsapp/MyfirtsappApplication.java
@@ -0,0 +1,13 @@
+package com.agustinbollati.springboot.demo.myfirtsapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyfirtsappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyfirtsappApplication.class, args);
+	}
+
+}
