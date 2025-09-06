f3c02620a213b684df3293c5dc486645c19adc5e
Agustin Bollati <bollatiagustin@gmail.com>
2025-09-06 16:48:52 -0300

    feat: agregar component scanning

---
+++ 02-spring-core/02-component-scanning/src/main/java/com/agustinbollati/springcoredemo/common/Coach.java
@@ -0,0 +1,5 @@
+package com.agustinbollati.springcoredemo;
+
+public interface Coach {
+    String getDailyWorkout();
+}
---
+++ 02-spring-core/02-component-scanning/src/main/java/com/agustinbollati/springcoredemo/rest/DemoController.java
@@ -0,0 +1,19 @@
+package com.agustinbollati.springcoredemo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+    private final Coach myCoach;
+
+    // Si tienes un solo constructor es opcional colocar @Autowired
+    public DemoController(Coach theCoach) {
+        this.myCoach = theCoach;
+    }
+
+    @GetMapping("/dailyworkout")
+    public String getDailyWorkout() {
+        return this.myCoach.getDailyWorkout();
+    }
+}
