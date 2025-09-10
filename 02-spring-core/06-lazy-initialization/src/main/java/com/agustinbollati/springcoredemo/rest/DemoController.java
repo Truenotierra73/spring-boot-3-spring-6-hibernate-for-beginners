f1b329b6d1fda308896fbef677082344c067a54a
Agustin Bollati <bollatiagustin@gmail.com>
2025-09-10 15:59:05 -0300

    feat: agregar proyecto con ejemplo de uso de carga perezosa de beans

---
+++ 02-spring-core/06-lazy-initialization/src/main/java/com/agustinbollati/springcoredemo/rest/DemoController.java
@@ -0,0 +1,24 @@
+package com.agustinbollati.springcoredemo.rest;
+
+import com.agustinbollati.springcoredemo.common.Coach;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+    private final Coach myCoach;
+
+    @Autowired
+    public DemoController(@Qualifier("tennisCoach") Coach myCoach) {
+	    System.out.println("In constructor: " + getClass().getSimpleName());
+		this.myCoach = myCoach;
+	}
+
+    @GetMapping("/dailyworkout")
+    public String getDailyWorkout() {
+        return this.myCoach.getDailyWorkout();
+    }
+}
