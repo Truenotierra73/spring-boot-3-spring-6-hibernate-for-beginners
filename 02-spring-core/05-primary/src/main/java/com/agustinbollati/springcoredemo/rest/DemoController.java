004d472fc7f7ebd8d315a2409c770b59c8898abc
Agustin Bollati <bollatiagustin@gmail.com>
2025-09-10 14:11:13 -0300

    feat: agregar proyecto con ejemplo de uso de @Primary

---
+++ 02-spring-core/05-primary/src/main/java/com/agustinbollati/springcoredemo/rest/DemoController.java
@@ -0,0 +1,23 @@
+package com.agustinbollati.springcoredemo.rest;
+
+import com.agustinbollati.springcoredemo.common.Coach;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+    private final Coach myCoach;
+
+    @Autowired
+    public DemoController(Coach myCoach) {
+		this.myCoach = myCoach;
+	}
+
+    @GetMapping("/dailyworkout")
+    public String getDailyWorkout() {
+        return this.myCoach.getDailyWorkout();
+    }
+}
