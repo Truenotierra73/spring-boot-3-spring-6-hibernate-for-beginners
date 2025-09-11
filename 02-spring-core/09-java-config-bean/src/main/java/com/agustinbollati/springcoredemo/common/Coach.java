b2549f07631ad0d8f274abfade8d5b2b997737e3
Agustin Bollati <bollatiagustin@gmail.com>
2025-09-11 16:05:02 -0300

    feat: agregar proyecto con ejemplo de uso de java config bean (uso de @Configuration y @Bean)

---
+++ 02-spring-core/09-java-config-bean/src/main/java/com/agustinbollati/springcoredemo/common/Coach.java
@@ -0,0 +1,5 @@
+package com.agustinbollati.springcoredemo.common;
+
+public interface Coach {
+    String getDailyWorkout();
+}
---
+++ 02-spring-core/09-java-config-bean/src/main/java/com/agustinbollati/springcoredemo/config/SportConfig.java
@@ -0,0 +1,15 @@
+package com.agustinbollati.springcoredemo.config;
+
+import com.agustinbollati.springcoredemo.common.Coach;
+import com.agustinbollati.springcoredemo.common.SwimCoach;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SportConfig {
+
+	@Bean
+	public Coach swimCoach() {
+		return new SwimCoach();
+	}
+}
